3d5b48af6f8e1052ddc4f844baabe77d92ab2dee
Farell Kresnanda <kresnandaf@gmail.com>
2025-06-13 03:12:24 +0700

    third commit

--- app.js
+++ app.js
@@ -79,6 +79,10 @@ app.get('/profile/:section', (req, res) => {  // Perbaiki rute menjadi /profile/
   });
 });
 
+app.get('/profile.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'images', 'profile.jpg'));
+});
+
 // Start server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, '0.0.0.0', () => {
