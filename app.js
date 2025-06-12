9b0b121a792290934d0ddeab545c17020938500c
Farell Kresnanda <kresnandaf@gmail.com>
2025-06-12 22:14:09 +0700

    second commit

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.get('/:section', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
