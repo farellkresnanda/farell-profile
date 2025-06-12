7e6357a1b43f12697971906427828b843f0f01a3
Farell Kresnanda <kresnandaf@gmail.com>
2025-06-13 05:49:14 +0700

    third commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Set up EJS as view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('trust proxy', true);
 
 
 // Profile data (from your CV)
